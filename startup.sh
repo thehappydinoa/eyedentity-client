c0af916a889509e95f4a57099da9cfb268539079
Aidan Holland <thehappydinoa@gmail.com>
2018-12-15 11:37:36 -0500

    startup.sh

---
+++ startup.sh
@@ -0,0 +1,13 @@
+echo "Copying cache to .cache"
+cp -nrp /home/pi/cache/ /home/pi/.cache;
+cd /home/pi/eyedentity-client/;
+echo "Pulling from GitHub"
+git pull;
+echo "Updating dependencies"
+pip3 install -r requirements.txt
+echo "Rendering frames"
+python3 render_frames.py;
+echo "Copying .cache to cache"
+cp -nrp /home/pi/.cache/ /home/pi/cache;
+echo "Starting client.py"
+python3 client.py;
